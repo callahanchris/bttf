8a1dd2d32e837619d01fa036e9a0f4f24df6f0e7
Chris Callahan <callahan.c@gmail.com>
2014-11-23 22:01:09 -0500

    add image

--- js/components/ImageSection.js
+++ js/components/ImageSection.js
@@ -4,8 +4,10 @@ var ImageSection = React.createClass({
   render: function() {
     return (
       <div className='image-section'>
-        <img src="https://www.flickr.com/photos/anieto2k/6983298944" />
-        <p>"If my calculations are correct, when this baby hits 88 miles per hour... you're gonna see some serious shit."</p>
+        {this.props.activated ?
+          <img src="img/6983298944_9f55d1ee2c_z.jpg" /> :
+          <p>"If my calculations are correct, when this baby hits 88 miles per hour... you're gonna see some serious shit."</p>
+        }
       </div>
     );
   }
